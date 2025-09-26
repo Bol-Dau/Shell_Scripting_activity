83b4238ace088888e48058cece4cdf9e111de751
Skezauwase <s.uwase4@alustudent.com>
2025-09-26 03:46:26 -0700

    completed step1 vault_setup

---
+++ vault_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+vault1="$root/secure_vault"
+
+if [ ! -d "$vault1" ]; then
+mkdir -p "$vault1"
+fi
+
+
+echo "welcome to keys.txt to store encryption keys" > "$vault1/keys.txt"
+echo "welcome to secrets.txt to store confidential data" > "$vault1/secrets.txt"
+echo "welcome to logs.txt to store system logs" > "$vault1/logs.txt"
+
+echo "$vault1 was created successfully"
+
+ls -l "$vault1"
